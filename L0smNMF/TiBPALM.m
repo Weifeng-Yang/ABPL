8d732fbaa7d4129b873b89d7575a090f1e8d88b8
WeifengYang <85056442+Weifeng-Yang@users.noreply.github.com>
2024-08-17 15:09:21 +0800

    Add files via upload

---
+++ L0smNMF/TiBPALM.m
@@ -0,0 +1,64 @@
+%%  All parameters of this function are explained the same as 'main_Run_me' and 'ALGOchoose' functions
+%%  This algorithm comes from the paper: "Inertial Proximal Alternating Linearized Minimization (iPALM) for Nonconvex and Nonsmooth Problems"
+function [var,loss,timerun,bts,ats]=TiBPALM(var,ngmar,aa,maxiteropt,num,stopindex,r)
+%% initialization algorithm
+loss=[];
+timerun=[0];
+for i=1:num
+    varze{i}=zeros(size(var{i},1),size(var{i},2));
+end
+bts=[];
+varK=var;
+loss(1)=compute(var,num,ngmar);
+returnloss=norm(ngmar,"fro");
+varK1=var;
+
+t1=clock;
+for i=1:maxiteropt
+fprintf("%d\n",i); 
+%% This extrapolation parameter update strategy is the best choice in iPALM according to the numerical results.
+at=(i-1)/(i+2);
+bt=(i-1)/(i+2);
+%% Update parameters
+
+varK1=varK;
+varK=var;
+for j=1:num
+V=gradtibpalm(var,varK,varK1,at,bt,ngmar,j,num,r);
+[var{j},~]=PROX(varze{j},V,aa(j));
+end
+loss(i+1)=compute(var,num,ngmar);
+bts(i)=bt;
+ats(i)=at;
+
+
+
+
+
+%% Check if termination condition is met
+fprintf("TiBPALM\n");
+for j=1:num
+    fprintf("nonzero:%d\n",nnz(var{j}));  
+end
+t2=clock;
+timerun(i+1)=etime(t2,t1);
+ReeK=loss(i)/returnloss;
+Ree=loss(i+1)/returnloss;
+Res=abs(Ree-ReeK);
+fprintf("Rel：%d\n",Res);
+stop=stopcheck(timerun,stopindex);
+if(stop==1)
+    fprintf("Number of terminations：%d\n",i);
+    pause(4);
+    break;
+end
+end
+
+end
+
+
+%% The parameters update function of iPALM
+
+
+
+
---
+++ L0smNMF/gradtibpalm.m
@@ -0,0 +1,45 @@
+%% Gradient calculation function for iPALM
+function U=gradtibpalm(var,varK,varK1,at,bt,ngmar,n,num,r)
+  
+
+
+    if(n>1)
+    Z=eye(size(var{1},1),size(var{1},1));
+    end
+    if(n<num)
+    ZT=eye(size(var{n+1},1),size(var{n+1},1)); 
+    end
+
+    for i=1:n-1
+        Z=Z*varK{i};
+    end
+    for i=n+1:num
+        ZT=ZT*varK{i};
+    end
+
+
+
+    if(n==1)
+    ctemp=norm(ZT*ZT','fro');
+%     r=2*(at+bt)/ctemp+1;
+    ck=2*(at+bt)+ctemp;
+    ck=r*checkck(ck);
+    mar=var{n}*ZT;
+    U=var{n}-1/ck*((mar-ngmar)*ZT'+at*(varK{n}-var{n})+bt*(varK1{n}-varK{n}));  
+    elseif(n<num)
+    ctemp=norm(Z'*Z,'fro')*norm(ZT*ZT','fro');
+    ck=2*(at+bt)+ctemp;
+    ck=r*checkck(ck);
+    mar=Z*var{n}*ZT;
+    U=var{n}-1/ck*(Z'*(mar-ngmar)*(ZT)'+at*(varK{n}-var{n})+bt*(varK1{n}-varK{n}));  
+    elseif(n==num)
+    ctemp=norm(Z'*Z,'fro');
+    ck=2*(at+bt)+ctemp;
+    ck=r*checkck(ck);
+    mar=Z*var{n};
+    U=var{n}-1/ck*(Z'*(mar-ngmar)+at*(varK{n}-var{n})+bt*(varK1{n}-varK{n}));  
+    end
+
+
+end
+
